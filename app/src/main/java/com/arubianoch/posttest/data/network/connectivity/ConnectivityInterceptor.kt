2e727aa81cfdb55d80c9f2e3d90754ae5f045cff
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-05-28 11:14:35 -0500

    Added class which will handle if device has internet connection

---
+++ app/src/main/java/com/arubianoch/posttest/data/network/connectivity/ConnectivityInterceptor.kt
@@ -0,0 +1,8 @@
+package com.arubianoch.posttest.data.network.connectivity
+
+import okhttp3.Interceptor
+
+/**
+ * @author Andres Rubiano Del Chiaro
+ */
+interface ConnectivityInterceptor : Interceptor
